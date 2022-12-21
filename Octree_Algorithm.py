308a59b2426d7140fc01c5a4f66ce1a0ab72dfb6
TomasLit <107041060+TomasLit@users.noreply.github.com>
2022-12-21 13:35:00 +0200

    Screenshots added

--- Octree_Algorithm.py
+++ Octree_Algorithm.py
@@ -1,6 +1,14 @@
 """
-The task is to implement a modified Octree algorithm. The algorithm should construct a standard Octree, but we want you to embed a sphere in each of the Octree cubes. 
-All the points within a sphere should further be subdivided into eight cubes (with spheres inside). All the points that are outside of the sphere can be discarded. Visualize the results.
+The task is to implement a modified Octree algorithm. The algorithm should construct a standard Octree two times. Meaning at first it should divide the X, Y, Z field into 8 cubes and then again divide
+those cubes into even smaller 8 cubes. Meaning 64 small cubes in the end. A sphere should be embedded into each of those 64 cubes. All the points that are outside of the sphere should be discarded. 
+The final result should be visualized.
+
+The program opens .las format file with coordinates of the points. Then it calculates the nearest and the furthest points from the starting point of the coordinates and from that moment it takes in
+the consideration only this field, as there could be a situation where the points are quite far from the starting point and further manipulations could give no or insignificant results. 
+Then the program divides the new field into 8 cuboids (yes, cuboids, not cubes as the length of the axis could differ by a lot, and again we would not get a significant result). This step is done to save 
+time as for computer, it would take less time to check the results divided into 8 smaller databases than a single big one.
+Then those 8 cuboids are divided into 8 smaller cuboids. After that, the program checks what points from the initial .las file are inside the spheres and what are not and writes the result in newly 
+generated "output.las" file. Visualization of the result could be generated using "Octree_algorithm_visualization.ipynb" file that is also attached.
 """
 
 import numpy as np
@@ -14,7 +22,8 @@ print("The program has started!")
 
 # Reading a .las file.
 print("Starting to read the .las file")
-las = laspy.read('2743_1234.las')
+# If you want a .las format file to open, you have to write the name of the file below.
+las = laspy.read('XXX.las')
 coordinates = np.stack((las.x, las.y, las.z)).transpose()
 print("Finished reading .las file in", " %s seconds " % round(time.time() - start_time, 3))
 start_time2 = time.time()
